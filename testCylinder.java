5296ed8fa89c44f5ef3efca1ca2286eada4e3a71
dinhhailinh <58936308+dinhhailinh@users.noreply.github.com>
2020-06-18 13:33:32 +0700

    Update testCylinder.java

--- testCylinder.java
+++ testCylinder.java
@@ -4,7 +4,7 @@ public class testCylinder {
     public static void main(String[] args) {
         point p = new point(3, 4);
         p.getInfor();
-        circle c = new circle(3);
+        circle c = new circle(3,4,3);
         c.Circle();
         cylinder C = new cylinder(6);
         C.Cylinder();
