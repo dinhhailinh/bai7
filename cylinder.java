0892d66a0085879223dc4dc1d4c753ef5f70a318
dinhhailinh <58936308+dinhhailinh@users.noreply.github.com>
2020-06-18 13:28:08 +0700

    Update cylinder.java

--- cylinder.java
+++ cylinder.java
@@ -32,7 +32,7 @@ public class cylinder extends circle {
         return super.area(r)*h;
     }
     public void Cylinder(){
-        System.out.println("The tich la: "+area(r));
+        System.out.println("The tich la: "+volume(r,h));
         System.out.println("Dien tich xung quanh la: "+areaAround(r, h));
     }
 }
